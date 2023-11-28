7ac535af2ae1145b6ea5fe1c5dd59540446801f4
Carlos Hernández <charlieappdev@outlook.com>
2023-11-27 22:19:48 -0600

    Implementing [Factory] and [Models]

---
+++ Model/Services/Factory.cs
@@ -0,0 +1,19 @@
+﻿namespace Packing.Model;
+
+public class Factory : IFactory
+{
+    public ILabel NewLabel(int number) =>
+        new Label(number);
+
+    public ILabelPack NewLabelPack(int minor, int major) =>
+        new LabelPack(minor, major);
+
+    public ILoteParameters NewLoteParameters(int palletsByLote, int levelsByPallet, int boxesByLevel) =>
+        new LoteParameters(palletsByLote, levelsByPallet, boxesByLevel);
+
+    public IPallet NewPallet(int number, int levels, int boxes) =>
+        new Pallet(number, levels, boxes);
+
+    public IPalletProperties NewPalletProperties(int levels, int boxes) =>
+        new PalletProperties(levels, boxes);
+}
