4f5fe154b2a363e91ad1aa311ac0ce6c7ed566ff
Carlos Hernández <charlieappdev@outlook.com>
2023-11-28 22:19:45 -0600

    Last File of "Renaming old project [Model]"

---
+++ ModelFirst/Services/Validation/RangeValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace Packing.Model.Services;
+
+public class RangeValidator : IValidators
+{
+    public bool IsValid(ILabel value, ILabelPack context) =>
+        IsInside(value.Number, new LabelRange(context));
+
+    public bool IsValid(IPalletProperties value, ILoteParameters context) =>
+        IsInside(value.Levels, new PalletLevelsRange(context)) &&
+        IsInside(value.Boxes, new LevelBoxesRange(context));
+
+    bool IsInside(int value, IRange range) =>
+        range.Min <= value && value <= range.Max;
+}
