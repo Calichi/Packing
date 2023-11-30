a2dbee28fc43b125d8947e588f50c328a91ca87f
Carlos Hernández <charlieappdev@outlook.com>
2023-11-30 00:59:43 -0600

    Calculator created

---
+++ Packing.Model/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Packing;
+
+public class Calculator(Context.IBundle context,
+                        Service.IPalletOperation palletOp,
+                        Factory.IUnits unitFactory,
+                        Factory.IContexts contextFactory,
+                        Service.IConverter converter)
+{
+    public Context.ILabelPack ToLabelPack(Unit.IPalletProperties palletProps) {
+        var major = converter.ToLabel(palletProps).Number - 1;
+        return contextFactory.NewLabelPack(2, major);
+    }
+}
