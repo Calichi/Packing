f551ac3c576d0a11ee27f43cf03e2c8cb2d16489
Carlos Hernández <charlieappdev@outlook.com>
2023-12-04 15:09:29 -0600

    Approved tests in Packing.Labels

---
+++ Packing.Testing/Label.Model.cs
@@ -0,0 +1,4 @@
+﻿namespace Packing.Testing;
+
+readonly record struct Pack(int MinorNumber, int MajorNumber) : Labels.IPack;
+readonly record struct Label(int Number) : Labels.ILabel;
--- Packing.Testing/Program.cs
+++ Packing.Testing/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Packing.Labels;
+using Packing.Testing;
+
+int number = 61;
+Pack pack = new(2, number);
+Label label = new(number);
+
+Console.WriteLine(pack);
+Console.WriteLine(label);
+Console.WriteLine($"Pending boxes: {label.GetPendingBoxes(pack)}");
+Console.WriteLine($"Produced boxes: {label.GetProducedBoxes(pack)}");
+Console.ReadLine();
