3d3fa678ca38de5f7eae468c3323c99a75877f7e
Carlos Hernández <charlieappdev@outlook.com>
2023-12-04 06:18:30 -0600

    add validation system

---
+++ Packing.Pallet/Validator.cs
@@ -0,0 +1,11 @@
+﻿namespace Packing.Pallet;
+
+public static class Validator
+{
+    public static bool IsValid(this IProperties props, IParameters parameters) =>
+        props.Levels > -1 &&
+        props.Boxes > -1 &&
+        props.Levels <= parameters.LevelsPerPallet &&
+        props.Boxes <= parameters.BoxesPerLevel &&
+        props.GetPendingBoxes(parameters) > -1;
+}
