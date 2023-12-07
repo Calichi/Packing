0520dbe2b4d4b34e97dff33b247c98c93f542a39
Carlos Hernández <charlieappdev@outlook.com>
2023-12-07 16:50:58 -0600

    New Funcionality

--- Packing.Convertion/PackingModels.cs
+++ Packing.Convertion/PackingModels.cs
@@ -2,3 +2,4 @@
 
 readonly record struct Label(int Number) : Labels.ILabel;
 readonly record struct Pallet(int Levels, int Boxes) : Pallets.IPallet;
+readonly record struct Pack(int MinorNumber, int MajorNumber) : Labels.IPack;
