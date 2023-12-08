228c20aa06406a2769e051366c41d8c15c9c7315
Carlos Hernández <charlieappdev@outlook.com>
2023-12-07 21:06:29 -0600

    Renaming Packing.Testing to Packing.Models and Changing dependency Packing by Packing.Model

--- Packing.Testing/Models.cs
+++ Packing.Testing/Models.cs
@@ -1,7 +1,9 @@
-﻿namespace Packing.Testing;
+﻿using Packing.Model;
 
-public readonly record struct Label(int Number) : Labels.ILabel;
-public readonly record struct Pallet(int Levels, int Boxes) : Pallets.IPallet;
-public readonly record struct Pack(int MinorNumber, int MajorNumber) : Labels.IPack;
-public readonly record struct PalletParameters(int LevelsPerPallet, int BoxesPerLevel) : Pallets.IParameters;
-public readonly record struct Parameters(Pallets.IParameters Pallet, Labels.IPack Label) : Convertion.IParameters;
+namespace Packing.Models;
+
+public readonly record struct Label(int Number) : ILabel;
+public readonly record struct Pallet(int Levels, int Boxes) : IPallet;
+public readonly record struct LabelParameters(int MinorNumber, int MajorNumber) : ILabelParameters;
+public readonly record struct PalletParameters(int LevelsPerPallet, int BoxesPerLevel) : IPalletParameters;
+public readonly record struct ConverterParameters(IPalletParameters Pallet, ILabelParameters Label) : IConverterParameters;
