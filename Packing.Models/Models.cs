28a496476d92c5dadeafa3435da82850ee279b58
Carlos Hernández <charlieappdev@outlook.com>
2023-12-11 22:05:15 -0600

    Deleting project Packing.Model

--- Packing.Models/Models.cs
+++
@@ -1,9 +0,0 @@
-﻿using Packing.Model;
-
-namespace Packing.Models;
-
-public readonly record struct Label(int Number) : ILabel;
-public readonly record struct Pallet(int Levels, int Boxes) : IPallet;
-public readonly record struct LabelParameters(int MinorNumber, int MajorNumber) : ILabelParameters;
-public readonly record struct PalletParameters(int LevelsPerPallet, int BoxesPerLevel) : IPalletParameters;
-public readonly record struct ConverterParameters(IPalletParameters Pallet, ILabelParameters Label) : IConverterParameters;
