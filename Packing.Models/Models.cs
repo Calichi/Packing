f109e9c1db8c0ac4bf14fb0fd1fde87ac406432c
Carlos Hernández <charlieappdev@outlook.com>
2023-12-12 11:50:09 -0600

    Defining new project Packing.Models

---
+++ Packing.Models/Models.cs
@@ -0,0 +1,28 @@
+﻿using Packing.Tracing;
+
+namespace Packing.Models;
+
+public readonly record struct
+    Label(int Number) : Tracing.ILabel;
+
+
+public readonly record struct
+    Pallet(int Number,
+           int Levels,
+           int Boxes) : Tracing.IPallet;
+
+
+public readonly record struct
+    LabelParameters(int MinorNumber,
+                    int MajorNumber) : Model.ILabelParameters;
+
+
+public readonly record struct
+    PalletParameters(int LevelsPerPallet,
+                     int BoxesPerLevel,
+                     int BoxesPerPallet) : Model.IPalletParameters;
+
+
+public readonly record struct
+    LoteParameters(Model.ILabelParameters Label,
+                   Model.IPalletParameters Pallet) : ILoteParameters;
