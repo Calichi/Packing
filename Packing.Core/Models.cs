2f1027b0b9c501877f9342efebd16e6fab3923c3
Carlos Hernández <charlieappdev@outlook.com>
2023-12-12 11:18:56 -0600

    Renaming ProductionReport

--- Packing.Core/Models.cs
+++ Packing.Core/Models.cs
@@ -5,4 +5,4 @@ namespace Packing.Core;
 readonly record struct Label(int Number) : ILabel;
 readonly record struct Pallet(int Levels, int Boxes) : IPallet;
 readonly record struct LabelParameters(int MinorNumber, int MajorNumber) : ILabelParameters;
-readonly record struct ProductionReport(int ProducedBoxes, int PendingBoxes) : IProductionReport;
+readonly record struct Boxes(int Produced, int Pending) : IBoxes;
