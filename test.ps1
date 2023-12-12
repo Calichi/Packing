91e1be5fe5aebc31ec38a1e79b2fbc63045ea7e5
Carlos Hernández <charlieappdev@outlook.com>
2023-12-12 12:14:26 -0600

    Making tests for Packing.Tracing

--- test.ps1
+++ test.ps1
@@ -1,9 +1,13 @@
 using namespace Packing.Models;
 
-Import-Project Packing.Model, Packing.Core, Packing.Models
+Import-Project Packing.Model,
+               Packing.Core,
+               Packing.Tracing,
+               Packing.Models
 
 $palletParams = [PalletParameters]::new(10, 6);
 $labelParams = [LabelParameters]::new(2, 61);
-$converterParams = [ConverterParameters]::new($palletParams, $labelParams);
-$packer = [Packing.Core.Packer]::new($converterParams); 
-$l32 = [Label]::new(32);
+$loteParams = [LoteParameters]::new($palletParams, $labelParams);
+$beginPallet = [Pallet]::new(1, 5, 0);
+$endPallet = [Label]::new(1);
+$packer = [Packing.Tracing.Packer]::new($loteParams, $beginPallet, $endPallet);
